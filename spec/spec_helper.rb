d2854dbdd19a8becdab9a4fe6ca4ec94560825dc
krim <pasha.rod@mail.ru>
2017-06-09 10:55:29 +0300

    remove pry-byebug as dev dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require "active_model"
 require "rspec"
 require "humanizer"
-require "pry-byebug"
 
 I18n.load_path << File.expand_path("../humanizer.en.yml", __FILE__)
 I18n.load_path << File.expand_path("../humanizer.empty.yml", __FILE__)
