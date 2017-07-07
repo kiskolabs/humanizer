768d2abd34008c08f7e2f482457e7f5cc76f3ab6
Vesa Vänskä <vesa@vesavanska.com>
2017-07-07 12:32:07 +0300

    Updates tests using transpec

--- spec/humanizer_spec.rb
+++ spec/humanizer_spec.rb
@@ -16,11 +16,11 @@ describe Humanizer do
 
     it "adds questions and answers to the instances" do
       questions = @user.send(:humanizer_questions)
-      questions.count.should == 2
-      questions[0]["question"].should == "Two plus two?"
-      questions[0]["answers"].should == ["4", "four"]
-      questions[1]["question"].should == "Jack and Jill went up the..."
-      questions[1]["answer"].should == "hill"
+      expect(questions.count).to eq(2)
+      expect(questions[0]["question"]).to eq("Two plus two?")
+      expect(questions[0]["answers"]).to eq(["4", "four"])
+      expect(questions[1]["question"]).to eq("Jack and Jill went up the...")
+      expect(questions[1]["answer"]).to eq("hill")
     end
 
     context "when question localizations can't be found" do
@@ -48,17 +48,17 @@ describe Humanizer do
     context "id" do
 
       it "is a random index for the questions array" do
-        @user.should_receive(:humanizer_questions).and_return([1])
-        @user.humanizer_question_id.should == 0
+        expect(@user).to receive(:humanizer_questions).and_return([1])
+        expect(@user.humanizer_question_id).to eq(0)
       end
 
     end
 
     it "is retrieved based on the set id" do
-      @user.should_receive(:humanizer_question_id).and_return(0)
-      @user.humanizer_question.should == "Two plus two?"
-      @user.should_receive(:humanizer_question_id).and_return(1)
-      @user.humanizer_question.should == "Jack and Jill went up the..."
+      expect(@user).to receive(:humanizer_question_id).and_return(0)
+      expect(@user.humanizer_question).to eq("Two plus two?")
+      expect(@user).to receive(:humanizer_question_id).and_return(1)
+      expect(@user.humanizer_question).to eq("Jack and Jill went up the...")
     end
 
   end
@@ -68,10 +68,10 @@ describe Humanizer do
     it "is retrieved for a given id" do
       answers_for_id_0 = @user.send(:humanizer_answers_for_id, 0)
       answers_for_id_1 = @user.send(:humanizer_answers_for_id, 1)
-      answers_for_id_0.count.should == 2
-      answers_for_id_0.should include("4")
-      answers_for_id_0.should include("four")
-      answers_for_id_1.should == ["hill"]
+      expect(answers_for_id_0.count).to eq(2)
+      expect(answers_for_id_0).to include("4")
+      expect(answers_for_id_0).to include("four")
+      expect(answers_for_id_1).to eq(["hill"])
     end
 
   end
@@ -81,57 +81,57 @@ describe Humanizer do
     it "can be any of the answers" do
       @user.humanizer_question_id = 0
       @user.humanizer_answer = "4"
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
       @user.humanizer_answer = "four"
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
     end
 
     it "is case-insensitive" do
       @user.humanizer_question_id = 1
       @user.humanizer_answer = "HILL"
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
       @user.humanizer_answer = "hiLL"
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
     end
 
     it "cannot be nil" do
       @user.humanizer_question_id = 0
       @user.humanizer_answer = nil
-      @user.humanizer_correct_answer?.should be_false
+      expect(@user.humanizer_correct_answer?).to be_falsey
     end
 
     it "cannot be an answer that doesn't match" do
       @user.humanizer_question_id = 1
       @user.humanizer_answer = "slope"
-      @user.humanizer_correct_answer?.should be_false
+      expect(@user.humanizer_correct_answer?).to be_falsey
     end
 
     it "can have extra spaces in begin and end" do
       @user.humanizer_question_id = 0
       @user.humanizer_answer = "4"
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
       @user.humanizer_answer = " 4"
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
       @user.humanizer_answer = "4 "
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
       @user.humanizer_answer = " 4 "
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
     end
 
     it "is can be an answer when question(as string)" do
       @user.humanizer_question_id = "0"
       @user.humanizer_answer = "4"
-      @user.humanizer_correct_answer?.should be_true
+      expect(@user.humanizer_correct_answer?).to be_truthy
     end
 
     it "is cannot be an answer when question(as string) not exists" do
       @user.humanizer_question_id = "10_000"
-      @user.humanizer_correct_answer?.should be_false
+      expect(@user.humanizer_correct_answer?).to be_falsey
     end
 
     it "is cannot be an answer when question not exists" do
       @user.humanizer_question_id = 10_000
-      @user.humanizer_correct_answer?.should be_false
+      expect(@user.humanizer_correct_answer?).to be_falsey
     end
   end
 
@@ -139,27 +139,27 @@ describe Humanizer do
 
     it "sets humanizer_question_id with no params" do
       @user.change_humanizer_question
-      @user.instance_variable_get(:@humanizer_question_id).should_not be_nil
+      expect(@user.instance_variable_get(:@humanizer_question_id)).not_to be_nil
     end
 
     context "when passing in a value" do
 
       before(:each) do
         questions = double(:count => 4)
-        @user.stub(:humanizer_questions).and_return(questions)
-        @user.send(:humanizer_question_ids).should == [0,1,2,3]
+        allow(@user).to receive(:humanizer_questions).and_return(questions)
+        expect(@user.send(:humanizer_question_ids)).to eq([0,1,2,3])
       end
 
       it "removes the question from the possible questions" do
         @user.change_humanizer_question(2)
-        @user.send(:humanizer_question_ids).should == [0,1,3]
+        expect(@user.send(:humanizer_question_ids)).to eq([0,1,3])
       end
 
       it "reloads the questions when it runs out" do
         3.times { |i| @user.change_humanizer_question(i) }
-        @user.send(:humanizer_question_ids).should == [3]
+        expect(@user.send(:humanizer_question_ids)).to eq([3])
         @user.change_humanizer_question(3)
-        @user.send(:humanizer_question_ids).should == [0,1,2]
+        expect(@user.send(:humanizer_question_ids)).to eq([0,1,2])
       end
 
     end
